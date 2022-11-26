536de6c2310e6390e79d70136e28d365413cdc6a
LONGMANKE <mburunjoroge0@gmail.com>
2022-11-26 15:00:24 +0000

    flutter

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,26 +1,76 @@
-// // import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-// // import 'package:flutter_signin_button/button_view.dart';
-// // import 'package:flutter_signin_button/flutter_signin_button.dart';
-// // import 'package:food_app/providers/user_provider.dart';
-// // import 'package:food_app/screens/home/home_screen.dart';
-// // import 'package:google_sign_in/google_sign_in.dart';
-// // import 'package:provider/provider.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: Container(
+      body: Container(
         height: double.infinity,
         width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
               fit: BoxFit.cover, image: AssetImage('assets/background.png')),
-        )
-       )
-
+        ),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Container(
+              height: 400,
+              width: double.infinity,
+              // color: Colors.red,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('Sign in to contunue'),
+                    Text(
+                      'MBURU VEGES',
+                      // 'Vegi',
+                      style: TextStyle(
+                          fontSize: 50,
+                          color: Colors.white,
+                          shadows: [
+                            BoxShadow(
+                              blurRadius: 5,
+                              color: Colors.green.shade900,
+                              offset: Offset(3, 3),
+                            )
+                          ]),
+                    ),
+                    Column(
+                      children: [
+                        SignInButton(
+                          Buttons.Apple,
+                          text: "Sign in with Apple",
+                          onPressed: () {},
+                        ),
+                        SignInButton(
+                          Buttons.Google,
+                          text: "Sign in with Google",
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          'By signing in you are agreeing to our',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                        Text(
+                          'Terms and Pricacy Policy',
+                          style: TextStyle(
+                            color: Colors.grey[800],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]))
+        ]),
+      ),
     );
   }
 }
