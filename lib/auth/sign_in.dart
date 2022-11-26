af5e896ab01aab4f98cc68da4011c67df229a7f9
LONGMANKE <mburunjoroge0@gmail.com>
2022-11-26 12:47:11 +0000

    flutter pub upgrade --major-versions

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -6,121 +6,21 @@ import 'package:flutter/material.dart';
 // // import 'package:food_app/screens/home/home_screen.dart';
 // // import 'package:google_sign_in/google_sign_in.dart';
 // // import 'package:provider/provider.dart';
+class SignIn extends StatelessWidget {
+  const SignIn({super.key});
 
-// class SignIn extends StatefulWidget {
-//   @override
-//   _SignInState createState() => _SignInState();
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              fit: BoxFit.cover, image: AssetImage('assets/background.png')),
+        )
+       )
 
-// class _SignInState extends State<SignIn> {
-//   UserProvider userProvider;
-//   Future<void> _googleSignUp() async {
-//     try {
-//       final GoogleSignIn _googleSignIn = GoogleSignIn(
-//         scopes: ['email'],
-//       );
-//       final FirebaseAuth _auth = FirebaseAuth.instance;
-
-//       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-//       final GoogleSignInAuthentication googleAuth =
-//           await googleUser.authentication;
-
-//       final AuthCredential credential = GoogleAuthProvider.credential(
-//         accessToken: googleAuth.accessToken,
-//         idToken: googleAuth.idToken,
-//       );
-
-//       final User user = (await _auth.signInWithCredential(credential)).user;
-//       // print("signed in " + user.displayName);
-//       userProvider.addUserData(
-//         currentUser: user,
-//         userEmail: user.email,
-//         userImage: user.photoURL,
-//         userName: user.displayName,
-//       );
-
-//       return user;
-//     } catch (e) {
-//       print(e.message);
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     userProvider = Provider.of<UserProvider>(context);
-//     return Scaffold(
-//       body: Container(
-//         height: double.infinity,
-//         width: double.infinity,
-//         decoration: BoxDecoration(
-//           image: DecorationImage(
-//               fit: BoxFit.cover, image: AssetImage('assets/background.png')),
-//         ),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Container(
-//               height: 400,
-//               width: double.infinity,
-//               child: Column(
-//                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                 children: [
-//                   Text('Sign in to contunue'),
-//                   Text(
-//                     'Vegi',
-//                     style:
-//                         TextStyle(fontSize: 50, color: Colors.white, shadows: [
-//                       BoxShadow(
-//                         blurRadius: 5,
-//                         color: Colors.green.shade900,
-//                         offset: Offset(3, 3),
-//                       )
-//                     ]),
-//                   ),
-//                   Column(
-//                     children: [
-//                       SignInButton(
-//                         Buttons.Apple,
-//                         text: "Sign in with Apple",
-//                         onPressed: () {},
-//                       ),
-//                       SignInButton(
-//                         Buttons.Google,
-//                         text: "Sign in with Google",
-//                         onPressed: () async {
-//                           await _googleSignUp().then(
-//                             (value) => Navigator.of(context).pushReplacement(
-//                               MaterialPageRoute(
-//                                 builder: (context) => HomeScreen(),
-//                               ),
-//                             ),
-//                           );
-//                         },
-//                       ),
-//                     ],
-//                   ),
-//                   Column(
-//                     children: [
-//                       Text(
-//                         'By signing in you are agreeing to our',
-//                         style: TextStyle(
-//                           color: Colors.grey[800],
-//                         ),
-//                       ),
-//                       Text(
-//                         'Terms and Pricacy Policy',
-//                         style: TextStyle(
-//                           color: Colors.grey[800],
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+    );
+  }
+}
