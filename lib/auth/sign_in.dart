4950da15aeb63c1577b9f503d141f9888e5e0c6b
LONGMANKE <mburunjoroge0@gmail.com>
2022-11-30 16:53:39 +0000

    mailtrap

--- lib/auth/sign_in.dart
+++ lib/auth/sign_in.dart
@@ -1,9 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/button_view.dart';
+// import 'package:flutter_signin_button/button_view.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+// import 'package:food_app/providers/user_provider.dart';
+// import 'package:food_app/screens/home/home_screen.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+//  Future<void> _googleSignUp() async {
+//     try {
+//       final GoogleSignIn _googleSignIn = GoogleSignIn(
+//         scopes: ['email'],
+      
+//       final FirebaseAuth _auth = FirebaseAuth.instance;
+
+//       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+//       final GoogleSignInAuthentication googleAuth =
+//           await googleUser.authentication;
+
+//       final AuthCredential credential = GoogleAuthProvider.credential(
+//         accessToken: googleAuth.accessToken,
+//         idToken: googleAuth.idToken,
+//       );
+
+//       final User user = (await _auth.signInWithCredential(credential)).user;
+//       // print("signed in " + user.displayName);
+//       // userProvider.addUserData(
+//       //   currentUser: user,
+//       //   userEmail: user.email,
+//       //   userImage: user.photoURL,
+//       //   userName: user.displayName,
+//       // );
+
+//       return user;
+//     } catch (e) {
+//       print(e.message);
+//     }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +86,15 @@ class SignIn extends StatelessWidget {
                         SignInButton(
                           Buttons.Google,
                           text: "Sign in with Google",
-                          onPressed: () {},
+                          onPressed: () async {
+                            await _googleSignUp().then(
+                              (value) => Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => Homepage(),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                       ],
                     ),
@@ -73,4 +119,7 @@ class SignIn extends StatelessWidget {
       ),
     );
   }
+}
+
+class Homepage {
 }
